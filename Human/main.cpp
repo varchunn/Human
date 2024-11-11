98a8bce8b10539126ba236989eea7a96105a54dd
varchunn <varvaram186@gmail.com>
2024-11-11 23:31:06 +0200

    Initial Commit

---
+++ Human/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Human
+//
+//  Created by Варя Мінєнкова on 11.11.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
